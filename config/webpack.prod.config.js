8513f584bfee92f405b22d12d2c91de3efb517cf
leshikjanz <alex.tereshko@softeq.com>
2017-12-20 00:16:05 +0300

    chore: set up webpack config for CI

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TSLintPlugin = require('tslint-webpack-plugin');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = 'production';
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -12,7 +11,7 @@ module.exports = {
   entry: ['babel-polyfill', './src/app.js'],
   output: {
     path: path.resolve(__dirname, "../dist"),
-    publicPath: '/',
+    publicPath: './',
     filename: 'app.bundle.js'
   },
 
@@ -77,8 +76,22 @@ module.exports = {
   },
 
   plugins: [
-    new TSLintPlugin({
-      files: ['./src/**/*.ts', './src/**/*.tsx']
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      children: true,
+      minChunks: 2,
+      async: true,
+    }),
+
+    // OccurrenceOrderPlugin is needed for long-term caching to work properly.
+    // See http://mxs.is/googmv
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+
+    // Minify and optimize the JavaScript
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false, // ...but do not show warnings in the console (there is a lot of them)
+      },
     }),
     new CopyWebpackPlugin([
       { from: 'src/assets', to: 'assets' }
@@ -86,19 +99,25 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Custom template',
       filename: 'index.html',
+      template: './src/index.html',
       minify: {
-        collapseWhitespace: true
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
       },
-      hash: true,
-      template: './src/index.html'
+      inject: true
     }),
     new ExtractTextPlugin({
       filename: 'app.css',
       disable: false,
       allChunks: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
-    })]
+    })
+  ]
 };
