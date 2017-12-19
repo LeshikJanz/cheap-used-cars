9f591a802d70bf394d5d4fe2012a33fd6b48db5f
leshikjanz <alex.tereshko@softeq.com>
2017-12-19 23:54:39 +0300

    refactor: refactor webpack.config name

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,102 +1,104 @@
-// Important modules this config uses
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TSLintPlugin = require('tslint-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OfflinePlugin = require('offline-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = require('./webpack.base.babel')({
-  // In production, we skip all hot-reloading stuff
-  entry: [
-    'whatwg-fetch',
-    path.join(process.cwd(), 'app/app.js'),
-  ],
-
-  // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+module.exports = {
+  entry: ['babel-polyfill', './src/app.js'],
   output: {
-    filename: 'js/[name].[chunkhash].js',
-    chunkFilename: 'js/[name].[chunkhash].chunk.js',
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: '/',
+    filename: 'app.bundle.js'
   },
 
-  // We use ExtractTextPlugin so we get a separate CSS file instead
-  // of the CSS being in the JS and injected as a style tag
-  // cssLoaders: ExtractTextPlugin.extract({
-  //   fallbackLoader: 'style-loader',
-  //   loader: 'css-loader?modules&-autoprefixer&importLoaders=1!postcss-loader',
-  // }),
+  devtool: "source-map",
+
+  resolve: {
+    modules: ['src', 'node_modules'],
+    alias: {
+      modules: path.resolve('src/modules'),
+      styles: path.resolve('src/styles'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader'
+        ]
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader",
+          options: {
+            includePaths: ["src/"]
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(ico|eot|otf|webp|svg|pdf|ttf|woff(2)?)(\?.*)?$/,
+        use: 'file-loader'
+      }]
+  },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      children: true,
-      minChunks: 2,
-      async: true,
-    }),
-
-    // OccurrenceOrderPlugin is needed for long-term caching to work properly.
-    // See http://mxs.is/googmv
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-
-    // Merge all duplicate modules
-    new webpack.optimize.DedupePlugin(),
-
-    // Minify and optimize the JavaScript
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false, // ...but do not show warnings in the console (there is a lot of them)
-      },
-    }),
-
-    // Minify and optimize the index.html
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-      inject: true,
-    }),
-
-    // Extract the CSS into a separate file
-    // new ExtractTextPlugin('[name].[contenthash].css'),
-
-    // Put it in the end to capture all the HtmlWebpackPlugin's
-    // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
-    new OfflinePlugin({
-      relativePaths: false,
-      publicPath: '/',
-
-      // No need to cache .htaccess. See http://mxs.is/googmp,
-      // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
-
-      caches: {
-        main: [':rest:'],
-
-        // All chunks marked as `additional`, loaded after main section
-        // and do not prevent SW to install. Change to `optional` if
-        // do not want them to be preloaded at all (cached only when first loaded)
-        additional: ['js/*.chunk.js'],
-      },
-
-      // Removes warning for about `additional` section usage
-      safeToUseOptionalCaches: true,
-
-      AppCache: false,
+    new TSLintPlugin({
+      files: ['./src/**/*.ts', './src/**/*.tsx']
     }),
     new CopyWebpackPlugin([
-      {
-        from: path.join(process.cwd(), '../package.json'),
-        to: path.join(process.cwd(), 'build/package.json')
-      }
+      { from: 'src/assets', to: 'assets' }
     ]),
-  ],
-});
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disable: false,
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
+    })]
+};
