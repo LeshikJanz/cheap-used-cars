d123b5864c8966fc04b68bf9f866feafe0341aaa
leshikjanz <alex.tereshko@softeq.com>
2017-12-22 16:28:13 +0300

    feat: implement search bar

--- src/modules/main/components/searchBar.tsx
+++ src/modules/main/components/searchBar.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import '../styles/searchBar.scss';
+const searchIcon = require('assets/icons/search.png');
 
 const SearchBar = () => (
   <div className="searchBarContainer">
-    <input type="text"/>
-    <button>Search</button>
+    <input type="text"
+           placeholder="Search for a model, or get started by choosing a colour, budget, or feature"
+    />
+    <button>
+      <label htmlFor="search">Search</label>
+      <img src={searchIcon} id="search" alt="Run search"/>
+    </button>
   </div>
 );
 
